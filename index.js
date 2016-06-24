916010726f816845fd737744d952740a8cc473c3
James Heazlewood <james.heazlewood@crownbet.com.au>
2016-06-24 14:08:56 +1000

    Added js and package files.

---
+++ index.js
@@ -0,0 +1,23 @@
+// Literally returns an empty stream object that can be pipe'd to the end of a a pipe
+module.exports = {
+    on: function() {},
+    removeListener: function() {},
+    addListener: function() {},
+    emit: function() {},
+    end: function() {},
+    read: function() {},
+    write: function() {},
+    cork: function() {},
+    uncork: function() {},
+    isPaused: function() {},
+    pause: function() {},
+    resume: function() {},
+    pipe: function() {},
+    unpipe: function() {},
+    shift: function() {},
+    unshift: function() {},
+    wrap: function() {},
+    push: function() {},
+    setEncoding: function() {},
+    once: function() {}
+};
